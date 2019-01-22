3b43624d0f15358bd41d1121145c2543248c3aa4
kyotaodagiri <kyotaodagiri@192.168.100.132>
2019-01-23 08:25:28 +0900

    Add MyPortflio

---
+++ MyPortfolio/src/main/java/com/example1/MyPortfolioApplication.java
@@ -0,0 +1,14 @@
+package com.example1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPortfolioApplication.class, args);
+	}
+
+}
+
